b8957193456c448d7e97d3a622a5af6e4e2b8582
lucas3ms <lucas3ms.ls@gmail.com>
2019-04-01 14:42:01 -0300

    Aplicando estilo a pagina

--- ProjetoVue/src/App.vue
+++ ProjetoVue/src/App.vue
@@ -1,31 +1,83 @@
 <template>
-<div>
-  <h1>{{ titulo }}</h1>
-  <ul>
-    <li v-for="foto of fotos" :key="foto.id">
-      <img :src="foto.url" :alt="foto.titulofoto">
-    </li>
-  </ul>
-</div>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.id">
+
+        <div class="painel"> 
+          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+          <div class="painel-conteudo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulofoto">
+          </div>
+        </div>    
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      titulo:'ProjetoVue',
+  data() {
+    return {
+      titulo: "ProjetoVue",
       fotos: []
-    }
+    };
   },
 
-  created(){
-    this.$http.get('http://localhost:3000/v1/fotos')
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
       .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
+      .then(fotos => (this.fotos = fotos), err => console.log(err));
   }
-}
+};
 </script>
 
 <style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva{
+    width: 100%;
+  }
+
+  /* estilo do painel */ 
+
+  .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 
 </style>
