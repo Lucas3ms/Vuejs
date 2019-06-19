7111102c1e4cc56c727f21bdc9f16b0975bfb89a
lucas3ms <lucas3ms.ls@gmail.com>
2019-06-19 09:59:12 -0300

    Criando componentes home e cadastro

---
+++ ProjetoVue/src/components/cadastro/cadastro.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="centralizado">Cadastro</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style>
+
+    .centralizado {
+        text-align: center;
+    }
+
+</style>
